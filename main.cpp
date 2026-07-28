67bbd739ed17ffa0a9ebd4e60354c3a86484d049
Andhika Rahman <andhikarhmnp@gmail.com>
2026-07-29 05:51:56 +0700

    create a rendering function (experimental)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 
+#define HEIGHT 25
+#define WIDTH 100
+
 struct Point
 {
     double x;
@@ -16,6 +19,8 @@ struct Object
     Object(const std::vector<Point> &object_points) : points(object_points) {};
 };
 
+void render(const Object &object);
+
 int main()
 {
     std::vector<Point> object_points = {
@@ -25,9 +30,20 @@ int main()
 
     Object A = object_points;
 
-    for(auto [x, y] : A.points) {
-        std::cout << x << ' ' << y << '\n';
-    }
+    render(A);
 
     return 0;
+}
+
+void render(const Object &object)
+{
+    for(int i = 0; i < HEIGHT; i++)
+    {
+        for(int j = 0; j < WIDTH; j++)
+        {
+            std::cout << '.';
+        }
+
+        std::cout << '\n';
+    }
 }
