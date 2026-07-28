3ac6bbcdccbfec66c154bfaefc697a04162e7aef
Andhika Rahman <andhikarhmnp@gmail.com>
2026-07-28 22:55:33 +0700

    create: a struct of point

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
+struct Point
+{
+    double x;
+    double y;
+
+    Point(double coord_x, double coord_y) : x(coord_x), y(coord_y) {};
+};
+
 int main()
 {
     std::cout << "Hello, world!\n";
